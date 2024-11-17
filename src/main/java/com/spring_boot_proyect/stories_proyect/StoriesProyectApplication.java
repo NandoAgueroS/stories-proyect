46182f3347e72dd49cc0b002b0e3714004f5baa7
Nando Javier Agüero <nandoaguerojs@gmail.com>
2024-11-16 23:17:49 -0300

    Initial commit

---
+++ src/main/java/com/spring_boot_proyect/stories_proyect/StoriesProyectApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot_proyect.stories_proyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoriesProyectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoriesProyectApplication.class, args);
+	}
+
+}
